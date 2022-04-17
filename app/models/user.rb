8e12f407954ac3a5fa17f0b99215414f2799be86
nisi0929 <kengo279@icloud.com>
2022-04-18 00:17:42 +0900

    local image post

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,25 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :validatable
+
+  has_one_attached :image
+
+  validates :image,
+            content_type: %i[gif png jpg jpeg],
+            size: {
+              less_than_or_equal_to: 5.megabytes,
+            },
+            dimension: {
+              width: {
+                max: 2000,
+              },
+              height: {
+                max: 2000,
+              },
+            }
 end
